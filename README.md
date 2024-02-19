e660fc2d2c0bbb8a3830b0dd13046972ba7c0395
MingxuanGame <MingxuanGame@outlook.com>
2024-02-19 14:08:45 +0800

    :sparkles: auto patch emoji on changed

--- README.md
+++ README.md
@@ -2,24 +2,9 @@
 
 Emoji 修复，使用系统的 Emoji 字体
 
-## 存在的兼容性问题
-
-与 [xiyuesaves/LiteLoaderQQNT-lite_tools](https://github.com/xiyuesaves/LiteLoaderQQNT-lite_tools) 不兼容。
-
-- 其“自定义字体”功能会覆盖本插件对 `document.body` 的注入。
-
-- 解决方案：
-    1. 禁用本插件；
-    2. 通过 [xiyuesaves/LiteLoaderQQNT-lite_tools](https://github.com/xiyuesaves/LiteLoaderQQNT-lite_tools) 的“自定义字体”功能修改字体。
-    QQ 默认字体的 Emoji 修复后的 `font-family` 如下
-
-    ```txt
-    system-ui, "PingFang SC", PingFangSC-Regular, "Microsoft YaHei", "Hiragino Sans GB", "Heiti SC", "WenQuanYi Micro Hei", Arial, Helvetica, sans-serif, "Apple Braille", "Color Emoji Fix"
-    ```
-
 ## 可能存在的兼容性问题
 
-可能与修改 CSS 的插件部分不兼容。
+本插件通过修改 `document.body` 的 `font-family` 实现修复，可能会与修改 `font-family` 的插件冲突。
 
 ## 许可证
 
--- renderer.js
+++ renderer.js
@@ -1,9 +1,31 @@
-function onload() {
-  document.body.style.fontFamily = window
-    .getComputedStyle(document.body)
+const dom = document.body;
+const observer = new MutationObserver(onChanged);
+
+function updateFonts() {
+  dom.style.fontFamily = window
+    .getComputedStyle(dom)
     .fontFamily.replace('"Color Emoji", ', "");
   console.log(
-    "[emojifix] Patched font-family to remove 'Color Emoji' font on document.body"
+    "[emojifix] Font updated. Current font-family: ",
+    dom.style.fontFamily
   );
+}
+
+function onChanged(mutations, _) {
+  for (var mutation of mutations) {
+    if (mutation.type === "attributes") {
+      updateFonts();
+    }
+  }
+}
+
+function onload() {
+  updateFonts();
+  observer.observe(dom, { attributeFilter: ["style"] });
+  setInterval(() => {
+    observer.disconnect();
+    updateFonts();
+    observer.observe(dom, { attributeFilter: ["style"] });
+  }, 1000);
 }
 onload();
